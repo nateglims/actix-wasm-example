12e07a0c98793e22917c9a1af3f5d300072501ed
Nathan Glimsdale <nglimsdale@gmail.com>
2019-04-13 16:57:22 -0500

    add simple backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,27 @@
+extern crate actix_web;
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
+use actix_web::middleware::Logger;
+use actix_web::{server, App, HttpRequest};
+
+fn index(_req: &HttpRequest) -> &'static str {
+    "Hello world!"
+}
+
 fn main() {
-    println!("Hello, world!");
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
+    info!("starting");
+
+    server::new(|| {
+        App::new()
+            .middleware(Logger::default())
+            .resource("/", |r| r.f(index))
+    })
+    .bind("127.0.0.1:8088")
+    .unwrap()
+    .run();
 }
